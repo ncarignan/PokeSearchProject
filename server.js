95f84e39b9a0b8c82d28fc46b7697975b45738a0
Nicholas Carignan <nick.carignan@sbcglobal.net>
2019-09-09 10:45:30 -0700

    linter errors and organization

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 //global's
 const express = require('express');
@@ -7,21 +7,26 @@ const pg = require('pg');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
-const url = 'https://pokeapi.co/api/v2/type/'
+
+const url = 'https://pokeapi.co/api/v2/type/';
 
 
 //middleware
+
+// These lines of code don't belong in global, they are also middleware
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 const methodOverride = require('method-override');
-app.use(methodOverride((req, res) =>{
-  if(req.body && typeof req.body === 'object' && '_method' in req.body){
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     let method = req.body._method;
-    delete req.body._method
+    delete req.body._method;
     return method;
   }
 }));
+
 // End of Middleware
 
 // connect to database
@@ -30,69 +35,69 @@ client.connect();
 client.on('error', (error) => console.error(error));
 
 // requests
-app.get('/', searchType)
-app.post('/search-query', askApi)
-app.post('/favorites', onePokemon)
+app.get('/', searchType);
+app.post('/search-query', askApi);
+app.post('/favorites', onePokemon);
 // app.post('/favorites' , deletePokemon)
 // app.post('/favorites' , pokemonDetails)
-app.post('/detail', showSinglePokemon)
-app.get('/favorites', onePokemon)
+app.post('/detail', showSinglePokemon);
+app.get('/favorites', onePokemon);
 
 
 
 ////////  constructor ////////
 function Pokemon(pokemon) {
-  this.name = pokemon.pokemon.name
+  this.name = pokemon.pokemon.name;
 }
 
 //////// Helper Functions ////////
 
 //save function
 function onePokemon(req, res) {
-  console.log('Deposited ', req.body.pokemonTyping, ' into pc')
+  console.log('Deposited ', req.body.pokemonTyping, ' into pc');
   // type_query is currently the favorites database
   client.query('INSERT INTO type_query (name) VALUES ($1)', [req.body.pokemonTyping]).then(loadFavorites(req, res))
     .catch(error => {
       res.render('./pages/error');
       console.error(error);
-    })
+    });
 }
 
 
-function searchType(req, res){
+function searchType(req, res) {
   res.render('./partials/search.ejs');
 }
 
-function askApi(req, res){
+function askApi(req, res) {
   const searchType = req.body.pokemonTyping.toLowerCase();
-  console.log('the search results include:', searchType)
+  console.log('the search results include:', searchType);
   const queryUrl = `${url}${searchType}`;
   console.log(queryUrl);
 
   //look at results
   superagent.get(queryUrl).then(result => {
-    const pokepath = result.body.pokemon
-    const newpokePath= pokepath.slice(0, 25).map(pokemon => {
-      return new Pokemon(pokemon)
+    const pokepath = result.body.pokemon;
+    const newpokePath = pokepath.slice(0, 25).map(pokemon => {
+      return new Pokemon(pokemon);
     });
     console.log('logging pokemon array', newpokePath);
-    res.render('partials/searchResult.ejs', {resultPokemon : newpokePath})
-  }).catch(error => console.error(error))
+    res.render('partials/searchResult.ejs', { resultPokemon: newpokePath });
+  }).catch(error => console.error(error));
 }
 
-function loadFavorites(req,res) {
+function loadFavorites(req, res) {
   client.query('SELECT * FROM type_query').then(resultFromdb => {
 
-    for(let i=0; i < resultFromdb.rows.length; i++){
-      console.log(' withdrawing ', resultFromdb.rows[i].name, ' from database')
+    for (let i = 0; i < resultFromdb.rows.length; i++) {
+      console.log(' withdrawing ', resultFromdb.rows[i].name, ' from database');
     }
-    res.render('./pages/favorites.ejs', {resultPokemon : resultFromdb.rows, rowCount : resultFromdb.rowCount});
+    res.render('./pages/favorites.ejs', { resultPokemon: resultFromdb.rows, rowCount: resultFromdb.rowCount });
     // console.log('I am logging resultFromdb.rows', resultFromdb.rows)
     // console.log('I am after the render')
   }).catch(error => {
     res.render('./pages/error');
     console.error(error);
-  })
+  });
 }
 //we should really put a .catch here
 
@@ -100,11 +105,11 @@ function loadFavorites(req,res) {
 function showSinglePokemon(req, res) {
   client.query('SELECT * FROM type_query WHERE id = $1', [req.body.pokesearch_app]).then(sqlResult => {
     // check that there is a valid result, show not found if not a valid result
-    res.render('./pages/pokemon/detail', {resultPokemon : resultFromdb.rows })
+    res.render('./pages/pokemon/detail', { resultPokemon: resultFromdb.rows });
   }).catch(error => {
     res.render('./pages/error');
     console.error(error);
-  })
+  });
 }
 
 //delete function
@@ -116,20 +121,20 @@ function deletePokemon(req, res) {
   }).catch(error => {
     res.render('./pages/error');
     console.error(error);
-  })
+  });
 }
 
 //update function
 function pokemonDetails(req, res) {
-  console.log(req.params)
+  console.log(req.params);
   client.query('SELECT * FROM type_query WHERE id = $1', [req.body.pokemonTyping]).then(sqlResult => {
     // check that there is a valid result, show not found if not a valid result
-    console.log(sqlResult.rows)
-    res.render('./pages/detials/edit', {resultPokemon : resultFromdb.rows })
+    console.log(sqlResult.rows);
+    res.render('./pages/detials/edit', { resultPokemon: resultFromdb.rows });
   }).catch(error => {
     res.render('./pages/error');
     console.error(error);
-  })
+  });
 }
 
 // ===== LISTENING ON ========
